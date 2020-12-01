91cc4c32e86242556f8acbf775852b32ee6cda16
Moonchild <moonchild@palemoon.org>
2020-12-01 14:11:57 +0000

    [nss] Import NSS 3.59/NSPR 4.29 source.

--- security/nss/coreconf/SunOS5.mk
+++ security/nss/coreconf/SunOS5.mk
@@ -35,13 +35,11 @@ DEFAULT_COMPILER = cc
 ifdef NS_USE_GCC
 	CC         = gcc
 	OS_CFLAGS += -Wall -Wno-format -Werror-implicit-function-declaration -Wno-switch
+	OS_CFLAGS += -D__EXTENSIONS__
 	CCC        = g++
 	CCC       += -Wall -Wno-format
 	ASFLAGS	  += -x assembler-with-cpp
 	OS_CFLAGS += $(NOMD_OS_CFLAGS) $(ARCHFLAG)
-	ifdef USE_MDUPDATE
-		OS_CFLAGS += -MDupdate $(DEPENDENCIES)
-	endif
 	ifdef BUILD_OPT
 	    OPTIMIZER = -O2
 	    # Enable this for accurate dtrace profiling
@@ -65,7 +63,7 @@ endif
 
 RANLIB      = echo
 CPU_ARCH    = sparc
-OS_DEFINES += -DSVR4 -DSYSV -D__svr4 -D__svr4__ -DSOLARIS -D_REENTRANT -D__EXTENSIONS__
+OS_DEFINES += -DSVR4 -DSYSV -D__svr4 -D__svr4__ -DSOLARIS -D_REENTRANT
 
 ifeq ($(OS_TEST),i86pc)
 ifeq ($(USE_64),1)
