b2eff33c9258cb5e0ce754f5a675885222212dba
Moonchild <moonchild@palemoon.org>
2022-05-19 21:57:58 +0000

    Issue #1210 - Introduce an internal API to get configured application locales.
    en-US will always be added as a fallback, to make things simpler down the line
    and avoid error checking all over the place.

--- chrome/nsChromeRegistryChrome.cpp
+++ chrome/nsChromeRegistryChrome.cpp
@@ -25,6 +25,7 @@
 
 #include "mozilla/LookAndFeel.h"
 #include "mozilla/Unused.h"
+#include "mozilla/intl/LocaleService.h"
 
 #include "nsICommandLine.h"
 #include "nsILocaleService.h"
@@ -385,6 +386,7 @@ nsresult nsChromeRegistryChrome::UpdateSelectedLocale()
       NS_ASSERTION(obsSvc, "Couldn't get observer service.");
       obsSvc->NotifyObservers((nsIChromeRegistry*) this,
                               "selected-locale-has-changed", nullptr);
+      mozilla::intl::LocaleService::GetInstance()->Refresh();
     }
   }
 
